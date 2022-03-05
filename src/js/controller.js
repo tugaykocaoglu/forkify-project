2c669dea801bdd9abcb3d3629ccd17d2238e9b06
tugaykocaoglu <tugay.kocaoglu@gmail.com>
2022-03-05 23:46:38 +0300

    new feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -144,5 +144,6 @@ const init = function () {
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
   newFeature();
+  newFeature();
 };
 init();
