e9cb399a851e037988cad4cdb2d57f6fb6fe3990
tugaykocaoglu <tugay.kocaoglu@gmail.com>
2022-03-05 22:15:25 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const API_KEY = '71a9cefb-16c2-44da-b45c-3d7fb51e7987';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const MODAL_CLOSE_SEC = 2.5;
